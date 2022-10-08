581dcf70e0efef73f2e753377fccb525103641c9
aravindsabu <aravindsabu2001@gmail.com>
2022-10-08 19:52:52 +0530

    on

---
+++ src/app/addeployee/addeployee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addeployee',
+  templateUrl: './addeployee.component.html',
+  styleUrls: ['./addeployee.component.css']
+})
+export class AddeployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
