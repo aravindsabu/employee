801093a45233c826b33bf8f94c7ca08b7bc85257
aravindsabu <aravindsabu2001@gmail.com>
2022-10-13 15:31:56 +0530

    ok

--- src/app/addeployee/addeployee.component.ts
+++ src/app/addeployee/addeployee.component.ts
@@ -8,6 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class AddeployeeComponent implements OnInit {
 
   constructor() { }
+  employeecode=""
+  name=""
+  desigination=""
+  gender=""
+  company=""
+  salary=""
+  address=""
+  number=""
+  email=""
+  experience=""
+  blood=""
+  birth=""
+
+  readValues=()=>{
+    let data={
+      " employeecode":this. employeecode,
+      " name":this.name,
+      "desigination":this.desigination,
+      "gender":this.gender,
+      "company":this.company,
+      "salary":this.salary,
+      "address":this.address,
+      "number":this.number,
+      "email":this.email,
+      "experience":this.experience,
+      "blood":this.blood,
+      "birth":this.birth
+    }
+    console.log(data)
+  }
 
   ngOnInit(): void {
   }
